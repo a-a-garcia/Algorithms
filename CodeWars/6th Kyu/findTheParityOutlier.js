060ea19b488f64e052fd00a04959eeff20f90060
Anthony A. Garcia <angarce01@gmail.com>
2023-09-10 18:00:34 -0700

    algo for 09/10/2023

--- CodeWars/6th Kyu/findTheParityOutlier.js
+++ CodeWars/6th Kyu/findTheParityOutlier.js
@@ -4,9 +4,30 @@ function findOutlier(integers){
     //first, need to decipher whether the majority of the numbers are even or odd
     //two counters, one for even one for odd
     //loop - if either counter is > 1, then we know the majority of the numbers are that counter
-    integers.filter(integer => {
-        if (integer )
-    })
+    let oddCounter = 0;
+    let evenCounter = 0;
+    for (let i = 0; i < integers.length; i++) {
+      if (integers[i] % 2 == 0) {
+        evenCounter++
+        if (evenCounter > 1) {
+          for (let j = 0; j <integers.length; j++) {
+            if (integers[j] % 2 !== 0) {
+              return integers[j]
+            }
+          }
+        }
+      } else if (integers[i] % 2 !== 0) {
+        oddCounter++
+        if (oddCounter > 1) {
+          for (let j = 0; j <integers.length; j++) {
+            if (integers[j] % 2 == 0) {
+              return integers[j]
+            }
+        }
+      }
+    }
   }
+}
 
-findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
