10806b0e76d1a8aa3d556395b141dfbf777e8f61
Anthony A. Garcia <angarce01@gmail.com>
2023-10-21 16:03:11 -0700

    Linked list practice

--- Udemy - DS&A/Section 8 - Linked Lists/Linked List Practice 1.js
+++ Udemy - DS&A/Section 8 - Linked Lists/Linked List Practice 1.js
@@ -33,18 +33,93 @@ class SinglyLinkedList {
 
     //ability to print the contents of the list
     printList() {
-        const linkedListAsArr = [];
-        let currentNode = this.head;
+        let linkedListAsArr = []
+        let currentNode = this.head
         while (currentNode !== null) {
             linkedListAsArr.push(currentNode.value)
             currentNode = currentNode.next
         }
         return linkedListAsArr
     }
+
+    prepend(value) {
+        const newNode = new Node(value)
+
+        let temp = this.head
+        this.head = newNode
+        this.head.next = temp
+
+        this.length++
+
+        return this //allows chaining
+    }
+
+    append(value) {
+        const newNode = new Node(value)
+
+        let temp = this.tail
+        this.tail.next = newNode
+        this.tail = temp
+
+        this.length++
+
+        return this
+    }
+
+    traverseToIndex(index) { //helper func that allows you to grab a single node from inside the linked list. assists with insert, delete...
+        let counter = 0
+        let currentNode = this.head
+        while (counter !== index) {
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value)
+        }
+
+        const newNode = new Node(value)
+
+        const leader = this.traverseToIndex(index - 1) //grab the node thats 1 BEFORE the index where you want to insert.
+
+        const temp = leader.next // store the leader's original next node in memory
+
+        leader.next = newNode // add the new node to the linked list, after the leader
+
+        newNode.next = temp // with the new node in the list now, make the new node's next node be the node in memory.
+
+        this.length++
+
+        return this
+    }
+
+    delete(index) {
+        const leader = this.traverseToIndex(index - 1) //again, grab the node thats 1 BEFORE the index you want to delete.
+        
+        const nodeToDelete = leader.next // store the location of the node you want to delete..
+
+        leader.next = nodeToDelete.next // this is where the actual deleting gets done, the leader's next becomes the nodeToDelete's next. because nodeToDelete is equal to leader.next, they just become equal to each other, "cutting out" or "forgetting" the node that was there originally.
+
+        this.length--
+
+        return this
+    }
 }
 
 const testSinglyLinkedList = new SinglyLinkedList(10)
 
+testSinglyLinkedList.prepend(2)
+
+testSinglyLinkedList.append(14)
+
+testSinglyLinkedList.insert(1, 219)
+
+testSinglyLinkedList.delete(2)
+
 console.log(testSinglyLinkedList.printList())
 
-console.log(testSinglyLinkedList)
+console.log(testSinglyLinkedList)   
+
